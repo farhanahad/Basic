f5d120975d0e0462c553852d06bba777e2d96f66
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-06-06 02:58:45 +0600

    Add files via upload

---
+++ (14)function overloading.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stdlib.h>
+#include<conio.h>
+using namespace std;
+void sum(int x,int y)
+{
+    int add=x+y;
+    cout<<add<<endl;
+}
+void sum(int x,int y,int z)
+{
+    int add=x+y+z;
+    cout<<add<<endl;
+}
+
+int main()
+{
+
+    sum(10,20);
+    sum(10,20,30);
+
+    getch();
+}
+
+
