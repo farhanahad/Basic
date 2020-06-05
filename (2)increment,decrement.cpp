6711e4d2f23a9c3eb0fdafbe4e6ebde661ec15f1
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-06-06 02:54:47 +0600

    Add files via upload

---
+++ (2)increment,decrement.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=6;
+    int y=--x;
+    cout<<x<<endl;
+    cout<<y<<endl;
+     int a=8;
+    int b=x--;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    int c=7;
+    int d=++x;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    int e=9;
+    int f=x++;
+    cout<<e<<endl;
+    cout<<f<<endl;
+
+
+
+}
