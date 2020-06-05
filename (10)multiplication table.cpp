aefc839b017bbbc0aeda7d368c19b36e7a01e104
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-06-06 02:57:18 +0600

    Add files via upload

---
+++ (10)multiplication table.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,i;
+    cout<<"Enter any number:";
+    cin>>num;
+
+    for( i=1;i<=10;i++)
+    {
+        cout<<num<<"X"<<i<<"="<<(num*i)<<endl;
+    }
+
+
+
+}
